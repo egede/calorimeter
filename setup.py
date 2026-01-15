e467c64d5ce61057fe59ecc5bd44de21f4fabafa
Ulrik Egede <ulrik.egede@monash.edu>
2026-01-16 10:12:01 +1100

    Fix setup script

--- setup.py
+++ setup.py
@@ -45,8 +45,7 @@ setup(
     include_package_data=True,
     data_files=[
         ("calorimeter/examples", [
-            "examples/*.ipynb",
-            "examples/*.py",
+            "calorimeterexamples/simulate.ipynb",
         ]),
     ],
 )
